6cba562276f07bd6fea02d69e0dca7cceca809b8
f3d4r <fedkinivn@gmail.com>
2025-04-24 14:16:27 +0300

    Commit changes

---
+++ source/OrderManager/OrderManager/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManager
+{
+    public class Order
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public DateTime TodayDate { get; set; }
+    }
+}
---
+++ source/OrderManager/OrderManager/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using OrderManager;
+
+public class Programm
+{
+    public static void Main()
+    {
+        Console.WriteLine( "Приветствуем в менеджере заказов" );
+        Console.WriteLine( "--------------------------------------------" );
+
+        Order order = new Order
+        {
+            ProductName = ReadProductName(),
+            Quantity = ReadQuantity(),
+            UserName = ReadUserName(),
+            Address = ReadAddress(),
+            TodayDate = DateTime.Now
+        };
+
+
+        OrderProcessor.ConfirmOrder( order );
+
+        Console.WriteLine( "--------------------------------------------" );
+        Console.WriteLine( "Спасибо за использование приложения. До свидания!" );
+    }
+
+    static string ReadUserName()
+    {
+        Console.Write( "Введите свое имя: " );
+        string userName = Console.ReadLine();
+        while ( string.IsNullOrWhiteSpace( userName ) )
+        {
+            Console.WriteLine( "Поле не может быть пустым" );
+            Console.Write( "Введите свое имя: " );
+            userName = Console.ReadLine();
+        }
+
+        return userName;
+    }
+
+    static string ReadProductName()
+    {
+        Console.Write( "Введите название товара: " );
+        string productName = Console.ReadLine();
+        while ( string.IsNullOrWhiteSpace( productName ) )
+        {
+            Console.WriteLine( "Поле не может быть пустым" );
+            Console.Write( "Введите название товара: " );
+            productName = Console.ReadLine();
+        }
+
+        return productName;
+    }
+
+    static int ReadQuantity()
+    {
+        Console.Write( "Введите количество товара: " );
+        int quantity;
+        string? input = Console.ReadLine();
+
+        while ( !ValidQuantity( input, out quantity ) )
+        {
+            Console.Write( "Введите корректное количество товара: " );
+            input = Console.ReadLine();
+        }
+
+        return quantity;
+    }
+
+    static bool ValidQuantity( string? input, out int quantity )
+    {
+        return int.TryParse( input, out quantity ) && quantity > 0;
+    }
+
+    static string ReadAddress()
+    {
+        Console.Write( "Введите адрес доставки: " );
+        string address = Console.ReadLine();
+        while ( string.IsNullOrWhiteSpace( address ) )
+        {
+            Console.WriteLine( "Поле не может быть пустым" );
+            Console.Write( "Введите адрес доставки: " );
+            address = Console.ReadLine();
+        }
+
+        return address;
+    }
+}
